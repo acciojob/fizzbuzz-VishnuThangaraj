cad9f81e447a2377842fb61d8e7938269830242e
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-10-11 21:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+result = "";
+
+for(let num=1; num<=100; num++){
+	
+	if(num % 3== 0 && num % 5 == 0)
+		result += "FizzBuzz\n";
+	else if(num % 3== 0)
+		result += "Fizz\n";
+	else if(num % 5 == 0)
+		result += "Buzz\n";
+	else
+		result += num+"\n";
+}
+
+alert(result);
